4ea8a92f0ee8073459b66db92d03079edbc13b66
Ubuntu <ubuntu@ip-172-31-0-194.us-west-1.compute.internal>
2022-07-08 03:10:58 +0000

    fixed student page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   root 'courses#index'
   get 'pages/about'
   get 'courses/new', to: 'courses#new'
+  resources :students
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
