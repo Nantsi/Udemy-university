7d15ff5d7da7f14cbbfedd6d15ff58743a0a4548
Ubuntu <ubuntu@ip-172-31-0-194.us-west-1.compute.internal>
2022-07-08 05:52:31 +0000

    Log in and log out functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,18 @@ def new
 end
 
 def create
+  student = Student.find_by(email: params[:logins][:email].downcase)
+  if student && student.authenticate(params[:logins][:password])
+    session[:student_id] = student.id
+    redirect_to student
+  else
+    render 'new'
+  end
 end
 
 def destroy
+  session[:student_id] = nil
+  redirect_to root_path
 end
 
   
